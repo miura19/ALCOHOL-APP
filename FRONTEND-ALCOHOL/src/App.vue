88ec686b2f24e689a58678ba32fa62ab5f17a36c
miura19 <littlecloud194@gmail.com>
2025-04-19 16:37:55 +0900

    tailwindcssインストール、Login.vueに処理転記

--- FRONTEND-ALCOHOL/src/App.vue
+++ FRONTEND-ALCOHOL/src/App.vue
@@ -1,72 +1,14 @@
-<script setup>
-import { ref } from 'vue'
-import axios from 'axios'
-
-const email = ref('')
-const password = ref('')
-const errorMessage = ref('')
-
-const handleLogin = async () => {
-	try {
-		// CSRF Cookieの取得
-		await axios.get('http://localhost/sanctum/csrf-cookie', { withCredentials: true })
-
-		const response = await axios.post('http://localhost/login', {
-			email: email.value,
-			password: password.value
-		}, {
-			withCredentials: true,
-			withXSRFToken: true,
-			headers: {
-				Accept: 'application/json',
-			}
-		})
-
-		console.log('ログイン成功:', response.data)
-		errorMessage.value = ''
-		// ここでrouterで遷移してもOK
-	} catch (error) {
-		console.error('ログイン失敗:', error)
-		errorMessage.value = 'ログインに失敗しました'
-	}
-}
-//認証済みならユーザーデータを取得
-const fetchUserData = async () => {
-	try {
-		const res = await axios.get('http://localhost/api/user', {
-			withCredentials: true,
-			withXSRFToken: true,
-			headers: {
-				Accept: 'application/json',
-			}
-		})
-		console.log('ユーザーデータ:', res.data)
-	} catch (error) {
-		console.error('ユーザーデータ取得失敗:', error)
-	}
-}
-
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-	<div class="login-form">
-		<h2>ログイン</h2>
-		<form @submit.prevent="handleLogin">
-			<div>
-				<label for="email">メールアドレス:</label>
-				<input type="email" id="email" v-model="email" required />
-			</div>
-			<div>
-				<label for="password">パスワード:</label>
-				<input type="password" id="password" v-model="password" required />
-			</div>
-			<button type="submit">ログイン</button>
-		</form>
-		<p v-if="errorMessage" style="color:red;">{{ errorMessage }}</p>
-		<button type="submit" @click="fetchUserData">ユーザー取得</button>
-	</div>
+
+  <RouterView />
 </template>
 
+<style scoped>
 
 
-<style scoped></style>
+</style>
---
+++ FRONTEND-ALCOHOL/src/views/LoginView.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+
+const email = ref('')
+const password = ref('')
+const errorMessage = ref('')
+
+const login = async () => {
+	try {
+		// CSRF Cookieの取得
+		await axios.get('http://localhost/sanctum/csrf-cookie', { withCredentials: true })
+
+		const response = await axios.post('http://localhost/login', {
+			email: email.value,
+			password: password.value
+		}, {
+			withCredentials: true,
+			withXSRFToken: true,
+			headers: {
+				Accept: 'application/json',
+			}
+		})
+
+		console.log('ログイン成功:', response.data)
+		errorMessage.value = ''
+		// ここでrouterで遷移してもOK
+	} catch (error) {
+		console.error('ログイン失敗:', error)
+		errorMessage.value = 'ログインに失敗しました'
+	}
+}
+
+//認証済みならユーザーデータを取得
+const fetchUserData = async () => {
+	try {
+		const res = await axios.get('http://localhost/api/user', {
+			withCredentials: true,
+			withXSRFToken: true,
+			headers: {
+				Accept: 'application/json',
+			}
+		})
+		console.log('ユーザーデータ:', res.data)
+	} catch (error) {
+		console.error('ユーザーデータ取得失敗:', error)
+	}
+}
+
+</script>
+
+<template>
+	<main>
+		<div class="bg-gray-100 min-h-screen flex items-center justify-center">
+			<div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
+				<h2 class="text-2xl font-bold mb-6 text-center text-gray-800">ログイン</h2>
+				<form action="#" method="POST" @submit.prevent="login">
+					<div class="mb-4">
+						<label for="email" class="block text-sm font-medium text-gray-700">メールアドレス</label>
+						<input v-model="email" type="email" id="email" name="email" placeholder="your@example.com" autocomplete="username" class="mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm">
+					</div>
+					<div class="mb-6 relative">
+						<label for="password" class="block text-sm font-medium text-gray-700">パスワード</label>
+						<div class="relative">
+							<input v-model="password" type="password" minlength="8" maxlength="16" id="password" name="password" placeholder="••••••••" autocomplete="current-password" class="mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm">
+						</div>
+					</div>
+					<button type="submit" class="w-full py-2 px-4 rounded-md shadow">ログイン</button>
+				</form>
+				<p v-if="errorMessage" style="color:red;">{{ errorMessage }}</p>
+				<p class="mt-4 text-cyan-800 underline"><RouterLink to="/register">新規ユーザー登録はこちら</RouterLink></p>
+				<button type="submit" @click="fetchUserData">ユーザー取得</button>
+			</div>
+		</div>
+	</main>
+</template>
+
+
+
+<style scoped></style>
