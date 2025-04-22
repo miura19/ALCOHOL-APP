838a1c174e3f703212f2137229fcc00b63e51224
DESKTOP-3FEQ971\wsatoh <wsatoh@s1.spiritek.co.jp>
2025-04-22 18:04:19 +0900

    忘れてた

--- BACKEND-ALCOHOL/routes/api.php
+++ BACKEND-ALCOHOL/routes/api.php
@@ -6,16 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-// Route::post('/login', function (Request $request) {
-//     $credentials = $request->validate([
-//         'email' => ['required', 'email'],
-//         'password' => ['required'],
-//     ]);
-
-//     if (Auth::attempt($credentials)) {
-//         $request->session()->regenerate();
-//         return response()->json(Auth::user());
-//     }
-//     return response()->json([], 401);
-// });
