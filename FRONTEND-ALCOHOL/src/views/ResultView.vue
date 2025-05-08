dc1464adb7af64f713e506bf5cd8ddbdcadc3eb4
DESKTOP-3FEQ971\wsatoh <wsatoh@s1.spiritek.co.jp>
2025-05-08 18:15:18 +0900

    ヘッダー追記

--- FRONTEND-ALCOHOL/src/views/ResultView.vue
+++ FRONTEND-ALCOHOL/src/views/ResultView.vue
@@ -91,5 +91,19 @@ const logout = async () => {
 </script>
 
 <template>
+    <header class="text-gray-600 body-font">
+		<div class="container mx-auto flex items-center justify-between p-5 flex-col md:flex-row">
+			<div class="flex title-font font-medium items-center text-gray-900">
+				<RouterLink to="/home">
+					<div class="ml-3 text-xl">ALCOLE-RECOMMENDATION</div>
+				</RouterLink>
+			</div>
+			<div class="flex items-center space-x-6">
+				<div class="font-medium text-gray-900 text-xl">{{ user?.name }}</div>
+				<button type="submit" @click="logout"
+					class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">ログアウト</button>
+			</div>
+		</div>
+	</header>
     Resutpage is here!
 </template>
