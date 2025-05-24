5a7703ea3f0371439529adae3c001cb460240c3a
miura19 <littlecloud194@gmail.com>
2025-05-25 01:19:44 +0900

    resultAPIの返却値を配列やめる

--- FRONTEND-ALCOHOL/src/views/ResultView.vue
+++ FRONTEND-ALCOHOL/src/views/ResultView.vue
@@ -93,13 +93,13 @@ const logout = async () => {
 		</div>			
 	</header>
 	<section class="text-gray-600 body-font">
-		<div class="container mx-auto flex px-5 pt-12	 items-center justify-center flex-col" v-if="resultData">
+		<div class="container mx-auto flex px-5 pt-12 items-center justify-center flex-col" v-if="resultData">
 			<div class="text-center lg:w-1/2 w-full">
 				<h1 class="title-font sm:text-2xl text-2xl mb-4 font-medium text-gray-800">あなたへのおすすめのお酒は</h1>
-				<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-800">{{ resultData[0]?.name }}</h1>
-				<p class="mb-8 mt-8 leading-relaxed text-xl">{{ resultData[0]?.description }}</p>
+				<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-800">{{ resultData?.name }}</h1>
+				<p class="mb-8 mt-8 leading-relaxed text-xl">{{ resultData?.description }}</p>
 				<div class="w-full">
-					<img class="mt-16 object-cover object-center rounded mx-auto" alt="お酒の画像" :src="resultData[0]?.image" width="500px" height="500px" />
+					<img class="mt-16 object-cover object-center rounded mx-auto" alt="お酒の画像" :src="resultData?.image" width="500px" height="500px" />
 				</div>
 			</div>
 		</div>
